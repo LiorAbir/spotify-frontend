82f9bcfd451c91e1d72764c943628b658605ba2b
Lior Abir <liorabir909@gmail.com>
2023-04-27 23:37:06 +0300

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { MainPage } from './views/MainPage'
 import { TopBar } from './cmps/TopBar'
 import { MainNav } from './cmps/MainNav'
+import { PlayingBar } from './cmps/PlayingBar'
 
 function App() {
 	return (
 		<Router>
 			<div className="app">
-				<div className="main-app grig main-layout">
+				<div className="main-app grid main-layout">
 					<TopBar />
 					<MainNav />
-					<Routes>
-						<Route path="/" element={<MainPage />}></Route>
-					</Routes>
+					<PlayingBar />
+					<main className="main-content">
+						<Routes>
+							<Route path="/" element={<MainPage />}></Route>
+						</Routes>
+					</main>
 				</div>
 			</div>
 		</Router>
---
+++ src/cmps/PlayingBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function PlayingBar() {
+	return (
+		<div className="playing-bar">
+			<footer className="playing-bar-container">
+				playlist bar
+				<div className="song-details"></div>
+				<div className="audio-track"></div>
+				<div className="audio-settings"></div>
+			</footer>
+		</div>
+	)
+}
