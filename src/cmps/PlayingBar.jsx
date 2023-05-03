dc1e42a46be374cccf076155602c15b5c7b73082
Lior Abir <liorabir909@gmail.com>
2023-05-03 12:15:07 +0300

    added basic content and styling to playing bar cmp

--- src/cmps/PlayingBar.jsx
+++ src/cmps/PlayingBar.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
+import { EmptyHeart } from '../svgs/EmptyHeart'
 
 export function PlayingBar() {
+	const onSetVolume = (e) => {
+		console.log(e)
+	}
 	return (
 		<div className="playing-bar">
-			<footer className="playing-bar-container">
-				playlist bar
-				<div className="song-details"></div>
-				<div className="audio-track"></div>
-				<div className="audio-settings"></div>
+			<footer className="playing-bar-container flex">
+				<div className="track-details flex">
+					<div>
+						<img
+							src="https://i.scdn.co/image/ab67616d00004851109b301a6526929293efbac2"
+							alt=""
+						/>
+					</div>
+					<div className="track-info">
+						<h4 className="track-name">והיא שעמדה - Live</h4>
+						<h6 className="tack-artist">
+							Akiva, Yonatan Razael, Nathan Goshen
+						</h6>
+					</div>
+					<button>
+						<EmptyHeart />
+					</button>
+				</div>
+
+				<div className="player-control">
+					<div className="player-btns"></div>
+					<div className="playback-bar">
+						<div className="time-left"></div>
+						<div className="player-progress-bar">
+							<input type="range" className="progress-bar" />
+						</div>
+						<div className="track-duration"></div>
+					</div>
+				</div>
+
+				<div className="audio-settings">
+					<div className="player-progress-bar">
+						<input
+							type="range"
+							min={0}
+							max={100}
+							className="progress-bar"
+							onMouseUp={(e) => onSetVolume(e)}
+						/>
+					</div>
+				</div>
 			</footer>
 		</div>
 	)
--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -3,19 +3,34 @@ import axios from 'axios'
 const YOUTUBE_KEY = import.meta.env.VITE_YOUTUBE_KEY
 
 export const youtubeService = {
-	query,
+	getTrackForDisplay,
 }
 
-query()
+getTrackForDisplay()
 
-async function query(filter = 'ישי ריבו') {
+async function getTrackForDisplay(filter = 'ישי ריבו') {
 	try {
 		const { data } = await axios.get(
 			`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_KEY}&q=${filter}`
 		)
 
+		let tracks = date.items.map(({ snippet, id }) => {
+			let { title, publishTime, description, thumbnails } = snippet
+			const { videoId } = id
+			const time = getTrackDuration(videoId)
+		})
+
 		console.log(data)
 	} catch (err) {
 		console.log(err)
 	}
+}
+
+async function getTrackDuration(songId) {
+	try {
+		const { data } = await axios.get(
+			`https://www.googleapis.com/youtube/v3/videos?id=${songId}&key=${YOUTUBE_KEY}&part=snippet,contentDetails,statistics,status`
+		)
+		let str = data.items[0]
+	} catch (err) {}
 }
